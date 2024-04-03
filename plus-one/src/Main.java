95da2ada36473d9a96b3836be1ab4340e58987e1
Pol Rubio Borrego <prubio@edu.tecnocampus.cat>
2024-04-03 23:21:56 +0200

    plus-one

---
+++ plus-one/src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] digits;
+        int[] result;
+
+        digits = new int[]{1, 2, 3};
+        result = solution.plusOne(digits);
+        for (int j : result)
+            System.out.print(j);
+        System.out.println("\n--------\n");
+
+        digits = new int[]{4, 3, 2, 1};
+        result = solution.plusOne(digits);
+        for (int j : result)
+            System.out.print(j);
+        System.out.println("\n--------\n");
+
+        digits = new int[]{9};
+        result = solution.plusOne(digits);
+        for (int j : result)
+            System.out.print(j);
+        System.out.println("\n--------\n");
+
+        digits = new int[]{9,8,7,6,5,4,3,2,1,0};
+        result = solution.plusOne(digits);
+        for (int j: result)
+            System.out.print(j);
+        System.out.println("\n--------\n");
+
+        digits = new int[]{7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
+        result = solution.plusOne(digits);
+        for (int j: result)
+            System.out.print(j);
+        System.out.println("\n--------\n");
+    }
+}
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        boolean done = false;
+        int i = digits.length-1;
+        while(!done){
+            if (i==-1) {
+                int[] digits2 = new int[digits.length+1];
+                digits2[0] = 1;
+                for(int j=1; j<digits2.length;j++)
+                    digits2[j] = digits[j-1];
+                return digits2;
+            }
+            if(digits[i]!=9){
+                digits[i] += 1;
+                done = true;
+            }
+            else{
+                digits[i] = 0;
+                i--;
+            }
+        }
+        return digits;
+    }
+}
